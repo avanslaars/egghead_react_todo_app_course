12f5aad97f5d8d781b1f5310190bcecd488457c3
Andrew L. Van Slaars <avanslaars@gmail.com>
2016-11-20 13:28:23 -0500

    Added static JSX for todos

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todos</h2>
         </div>
-        <p className="App-intro">
-          Hello, React!
-        </p>
+        <div className="Todo-App">
+          <form>
+            <input type="text"/>
+          </form>
+          <div className="Todo-List">
+            <ul>
+              <li><input type="checkbox"/> Learn JSX</li>
+              <li><input type="checkbox"/> Build an Awesome App</li>
+              <li><input type="checkbox"/> Ship It!</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
