3a8c169d6aacad7e7dab2633d0c2eecea2353e7a
Andrew L. Van Slaars <avanslaars@gmail.com>
2016-11-20 16:36:23 -0500

    Added initial state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {id: 1, name: 'Learn JSX', isComplete: true},
+        {id: 2, name: 'Build an Awesome App', isComplete: false},
+        {id: 3, name: 'Ship It', isComplete: false}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -16,9 +26,10 @@ class App extends Component {
           </form>
           <div className="Todo-List">
             <ul>
-              <li><input type="checkbox"/> Learn JSX</li>
-              <li><input type="checkbox"/> Build an Awesome App</li>
-              <li><input type="checkbox"/> Ship It!</li>
+              {this.state.todos.map(todo =>
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+                </li>)}
             </ul>
           </div>
         </div>
