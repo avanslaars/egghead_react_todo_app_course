d83d89bc206e2ce76b5178d1400609a24b660b28
Andrew L. Van Slaars <avanslaars@gmail.com>
2016-12-02 16:09:01 -0500

    Added conditional form handling

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     }
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
   }
 
   handleSubmit(evt) {
@@ -28,7 +29,13 @@ class App extends Component {
       todos: updatedTodos,
       currentTodo: ''
     })
+  }
 
+  handleEmptySubmit(evt) {
+    evt.preventDefault()
+    this.setState({
+      errorMessage: 'Please supply a todo name'
+    })
   }
 
   handleInputChange (evt) {
@@ -38,6 +45,7 @@ class App extends Component {
   }
 
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
     return (
       <div className="App">
         <div className="App-header">
@@ -45,9 +53,10 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
         <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
           <TodoForm handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
-            handleSubmit={this.handleSubmit}/>
+            handleSubmit={submitHandler}/>
           <TodoList todos={this.state.todos}/>
         </div>
       </div>
