ffac6123dd2ab11078e6ed25df751548fcb7fdc8
Andrew L. Van Slaars <avanslaars@gmail.com>
2016-12-16 20:55:47 -0500

    Added popstate handling to router

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -27,6 +27,12 @@ export class Router extends Component {
     }
   }
 
+  componentDidMount() {
+    window.onpopstate = () => {
+      this.setState({route: getCurrentPath()})
+    }
+  }
+
   render () {
     return <div>{this.props.children}</div>
   }
